572d791660b6e1846683c3dee05e5e9e278be087
maxwelll_house <ks.kuraeva@gmail.com>
2024-03-21 21:57:27 +0300

    comments and code-style

--- NotepadMain.cpp
+++ NotepadMain.cpp
@@ -35,8 +35,6 @@ void NotepadMain::on_actionOpen_triggered()
 
 void NotepadMain::on_actionSave_triggered()
 {
-   
-
     QString fileName = QFileDialog::getSaveFileName(this, "Save");
     QFile file(fileName);
     if (!file.open(QFile::WriteOnly | QFile::Text)) {
@@ -53,54 +51,29 @@ void NotepadMain::on_actionSave_triggered()
 
 void NotepadMain::on_actionSaveAs_triggered()
 {
-    // Opens a dialog for saving a file
     QString fileName = QFileDialog::getSaveFileName(this, "Save as");
-
-    // An object for reading and writing files
     QFile file(fileName);
-
-    // Try to open a file with write only options
     if (!file.open(QFile::WriteOnly | QFile::Text)) {
         QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
         return;
     }
-
-    // Store the currentFile name
     currentFile = fileName;
-
-    // Set the title for the window to the file name
     setWindowTitle(fileName);
-
-    // Interface for writing text
     QTextStream out(&file);
-
-    // Copy text in the textEdit widget and convert to plain text
     QString text = ui.textEdit->toPlainText();
-
-    // Output to file
     out << text;
-
-    // Close the file
     file.close();
 }
 
 void NotepadMain::on_actionPrint_triggered()
 {
-    // Allows for interacting with printer
     QPrinter printer;
-
-    // You'll put your printer name here
     printer.setPrinterName("Printer Name");
-
-    // Create the print dialog and pass the name and parent
     QPrintDialog pDialog(&printer, this);
-
     if (pDialog.exec() == QDialog::Rejected) {
         QMessageBox::warning(this, "Warning", "Cannot Access Printer");
         return;
     }
-
-    // Send the text to the printer
     ui.textEdit->print(&printer);
 }
 
